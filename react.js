d616a9916bae411041a19f4dadcde9e763cb6f6b
Monkey <duyk16@gmail.com>
2019-02-17 21:28:36 +0700

    fix break lines tag

--- react.js
+++ react.js
@@ -1,3 +1,49 @@
+const defaultInput = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 class App extends React.Component {
   state = {
     input: '',
@@ -7,13 +53,13 @@ class App extends React.Component {
   componentWillMount() {
     this.setState({
       input: defaultInput,
-      output: marked(defaultInput)
+      output: marked(defaultInput, {breaks: true})
     })
   }
   
   handleChange(e) {
     let input = e.target.value
-    let output = marked(input)
+    let output = marked(input, {breaks: true})
     this.setState({
       input,
       output
@@ -69,39 +115,3 @@ class Preview extends React.Component {
 
 React.createElement("div", {key: "value"}, )
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const defaultInput = `# Hello, This is Markdown Live Preview
-
-----
-## what is Markdown?
-see [Wikipedia](https://en.wikipedia.org/wiki/Markdown)
-
-> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
-
-----
-## usage
-1. Write markdown text in this textarea.
-2. Click 'HTML Preview' button.
-
-----
-## markdown quick reference
-# headers
-
-*emphasis*
-
-**strong**
-
-* list
-
->block quote
-
-    code (4 spaces indent)
-[links](https://wikipedia.org)
-
-----
-## changelog
-* 17-Feb-2013 re-design
-
-----
-## thanks
-`
